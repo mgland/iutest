16d912b8f8279012748d9b73169677dc0d1edc55
Miguel Gao <mgtoolspro@gmail.com>
2020-05-17 13:18:27 +1000

    Try to support PySide2,1 and PyQt4,5

--- utest/qt.py
+++ utest/qt.py
@@ -1,4 +1,54 @@
-from PySide2 import QtWidgets, QtCore, QtGui
+import logging
+
+logger = logging.getLogger(__name__)
+
+def _importFromPySide2():
+    try:
+        from PySide2 import QtCore, QtGui, QtWidgets
+        return (QtCore, QtGui, QtWidgets)
+    except Exception:
+        logger.debug('Unable to import PySide2.')
+    return (None, None, None)
+
+
+def _importFromPySide1():
+    try:
+        from PySide import QtCore, QtGui
+        return (QtCore, QtGui, QtGui)
+    except Exception:
+        logger.debug('Unable to import PySide.')
+    return (None, None, None)
+
+
+def _importFromPyQt5():
+    try:
+        from PyQt5 import QtCore, QtGui, QtWidgets
+        return (QtCore, QtGui, QtWidgets)
+    except Exception:
+        logger.debug('Unable to import PyQt5.')
+    return (None, None, None)
+
+
+def _importFromPyQt4():
+    try:
+        from PyQt4 import QtCore, QtGui
+        return (QtCore, QtGui, QtGui)
+    except Exception:
+        logger.debug('Unable to import PyQt4.')
+    return (None, None, None)
+
+
+def _importQtModules():
+    importers = (_importFromPySide2, _importFromPySide1, _importFromPyQt5, _importFromPyQt4)
+    for importer in importers:
+        QtCore, QtGui, QtWidgets = importer()
+        if QtCore:
+            return (QtCore, QtGui, QtWidgets)
+    
+    logger.error('No Qt modules found.')
+    return (None, None, None)
+    
+QtCore, QtGui, QtWidgets = _importQtModules()
 
 
 def findTopLevelWidgetByName(name):
