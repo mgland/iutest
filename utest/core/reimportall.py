bf4b122e4018570e74f9f134df8d923f0467ba68
Miguel Gao <mgtoolspro@gmail.com>
2020-05-21 22:31:12 +1000

    Catch exceptions during reimportall

--- utest/core/reimportall.py
+++ utest/core/reimportall.py
@@ -1,11 +1,17 @@
+import logging
 from utest.libs import reimport
 
+logger = logging.getLogger(__name__)
 
 def reimportAllChangedPythonModules():
     changed = reimport.modified()
     if changed:
-        print("Reimport modules: {}".format(changed))
-        reimport.reimport(*changed)
+        print("Start reimporting the changed modules: {}".format(changed))
+        for module in changed:
+            try:
+                reimport.reimport(module)
+            except Exception:
+                logger.exception('Unable to reimport module %s', module)
     else:
-        print("No changed modules!")
+        print("No changed modules to reimport :)")
     return changed
