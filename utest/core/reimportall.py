00c10425549203ae269c82248e744a28f0252330
Miguel Gao <mgtoolspro@gmail.com>
2020-05-28 22:24:25 +1000

    Improve reimportAll logging

--- utest/core/reimportall.py
+++ utest/core/reimportall.py
@@ -7,12 +7,13 @@ logger = logging.getLogger(__name__)
 def reimportAllChangedPythonModules():
     changed = reimport.modified()
     if changed:
-        print("Start reimporting the changed modules: {}".format(changed))
+        print("Reimporting: {}".format(changed))
         for module in changed:
             try:
                 reimport.reimport(module)
             except Exception:
                 logger.exception("Unable to reimport module %s", module)
+        print ("{} modules reimported.".format(len(changed)))
     else:
         print("No changed modules to reimport :)")
     return changed
